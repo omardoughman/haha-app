8e91e5923956de5784a383b709ceb1432d9d6dc8
omardoughman <238817456+omardoughman@users.noreply.github.com>
2026-05-07 11:25:04 +0100

    Create run-main.rb for project initialization
    This script serves as an entry point for the Ferrari M1 project and checks the CMake version.

---
+++ run-main.rb
@@ -0,0 +1,6 @@
+# run_project.rb
+puts "Pit Crew checking in!"
+puts "Starting the Ferrari M1 project..."
+
+# This Ruby script can eventually trigger your CMake or D builds
+system("cmake --version")
